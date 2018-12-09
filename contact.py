9f960998476f21bbecd1337b2d1c6dacab10fb10
Саракул Вячеслав <ssdrug1@gmail.com>
2018-12-09 09:02:19 +0200

    Добавил новый модуль с классом Contact перечисляющий все параметры формы создания нового контакта

---
+++ contact.py
@@ -0,0 +1,31 @@
+class Contact:
+    def _init_ (self, notes, first_name, middle_name, last_name, nickname, title, company_name, address_1,
+                      home_phone, cell_phone, work_phone, fax, email_1, email_2, email_3, home_page, birth_day,
+                      birth_month, birth_year, anniversay_day, anniversay_month, anniversay_year, part_of_group,
+                      address_2, suit_nr):
+
+        self.notes = notes
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.title = title
+        self.company_name = company_name
+        self.address_1 = address_1
+        self.home_phone = home_phone
+        self.cell_phone = cell_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.email_1 = email_1
+        self.email_2 = email_2
+        self.email_3 = email_3
+        self.home_page = home_page
+        self.birth_day = birth_day
+        self.birth_month = birth_month
+        self.birth_year = birth_year
+        self.anniversay_day = anniversay_day
+        self.anniversay_month = anniversay_month
+        self.anniversay_year = anniversay_year
+        self.part_of_group = part_of_group
+        self.address_2 = address_2
+        self.suit_nr = suit_nr
